2458457063cae071b5c7bdc139c7137306fb0c49
Ehsan <JMSEhsan@gmail.com>
2021-12-18 23:42:13 -0700

    Python_Booleans

---
+++ Python_Booleans.py
@@ -0,0 +1,11 @@
+# Python Booleans
+# https://www.w3schools.com/python/python_booleans.asp
+
+print("print(5 > 3) : ", 5 > 3)
+print("print(5 == 3):", 5 == 3)
+print("print(5 < 3)", 5 < 3)
+print()
+print("print(bool(5 < 3)): ", bool(5 < 3))
+print()
+print("print(bool(\"Hello\"):", bool("Hello"))
+print("print(bool(5):", bool(5))
