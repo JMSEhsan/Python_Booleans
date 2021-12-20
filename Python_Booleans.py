54f5a1f9ce85b671eaecee684b98c37d2c1cbf80
Ehsan <JMSEhsan@gmail.com>
2021-12-19 23:05:14 -0700

    commit, added more boolean practice

--- Python_Booleans.py
+++ Python_Booleans.py
@@ -2,10 +2,13 @@
 # https://www.w3schools.com/python/python_booleans.asp
 
 print("print(5 > 3) : ", 5 > 3)
-print("print(5 == 3):", 5 == 3)
-print("print(5 < 3)", 5 < 3)
+print("print(5 == 3) : ", 5 == 3)
+print("print(5 < 3): ", 5 < 3)
 print()
-print("print(bool(5 < 3)): ", bool(5 < 3))
+print("print(bool(5 < 3)) : ", bool(5 < 3))
 print()
-print("print(bool(\"Hello\"):", bool("Hello"))
-print("print(bool(5):", bool(5))
+print("print(bool(\"Hello\") : ", bool("Hello"))
+print("print(bool(5) : ", bool(5))
+print("print(bool(\"abc\") : ", bool("abc"))
+print("print(bool([\"orange\", \"cherry\"] : ", bool(["orange", "cherry"]))
+print("print(bool(False), bool(None), bool(0), bool(""), bool(), bool(()), bool([]), bool({})) : ", bool(False), bool(None), bool(0), bool(""), bool(), bool(()), bool([]), bool({}))
