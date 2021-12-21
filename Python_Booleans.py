83aaf96e521e6fcc5cf97cfa14a9dbbacab3ebf7
Ehsan <JMSEhsan@gmail.com>
2021-12-20 23:59:13 -0700

    commit, added prectice

--- Python_Booleans.py
+++ Python_Booleans.py
@@ -12,3 +12,11 @@ print("print(bool(5) : ", bool(5))
 print("print(bool(\"abc\") : ", bool("abc"))
 print("print(bool([\"orange\", \"cherry\"] : ", bool(["orange", "cherry"]))
 print("print(bool(False), bool(None), bool(0), bool(""), bool(), bool(()), bool([]), bool({})) : ", bool(False), bool(None), bool(0), bool(""), bool(), bool(()), bool([]), bool({}))
+
+# An object made from a class with a function that returns 0 or False
+
+class myclass():
+    def __len__(self):
+        return 0
+myobject = myclass()
+print("myobject value: ", bool(myobject))
